9c5bcff8b8c70f5c3c714c9894fff2e91e427207
AndrewKim330 <rendezvous330@gmail.com>
2022-07-26 23:15:06 +0900

    chore: Add miscellanous studying feature (220726)

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,11 @@ const good = {
 };
 
 // good.cang() -> error occurs. cannot be compiled
+
+// [1, 2, 3] + false -> type error
+
+const remainder = (a: number, b: number) => {
+    return a % b;
+};
+
+// remainder(3) -> error for number of arguments
