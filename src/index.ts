e7d52b6fb41a32f6b5696d3856eef3a303a1d7c3
AndrewKim330 <rendezvous330@gmail.com>
2022-07-28 20:13:18 +0900

    feat: Study NomadCoder lecture (Basic of Typescript, 220728)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { PassThrough } from 'stream';
+
 // 220726
 const good = {
     bang: 'root',
@@ -29,3 +31,58 @@ c.push(3);
 const d = { a: 3 };
 // d.a = 'a'; -> expected to error
 d.a = 4;
+// d.good; -> expected to error
+
+// 220728
+
+const noGood: object = {
+    name: 'Kim',
+};
+
+// noGood.name; -> expected to error
+
+const noGood2: {
+    name: string;
+    // age: number
+    age?: number;
+} = {
+    name: 'kim',
+};
+
+// if (noGood2.age > 10) {} -> expected to error
+if (noGood2.age && noGood2.age > 10) {
+}
+
+type Player = {
+    name: string;
+    age?: number;
+};
+
+const p1: Player = {
+    name: 'Kim',
+};
+
+const p2: Player = {
+    name: 'Lee',
+    age: 18,
+};
+
+const playerMaker1 = (name: string) => {
+    return {
+        name,
+    };
+};
+
+const p3 = playerMaker1('Park');
+// p3.age = 18 -> expected to error
+
+const playerMaker2 = (name: string): Player => {
+    return {
+        name,
+    };
+};
+
+const p4 = playerMaker2('Choi');
+p4.age = 18;
+
+//
